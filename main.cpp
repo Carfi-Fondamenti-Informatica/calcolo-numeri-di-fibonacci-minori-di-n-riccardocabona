f224077eafe44a63c34c4760cd67ff10999b31b0
riccardocabona <91782134+riccardocabona@users.noreply.github.com>
2021-11-14 14:17:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n=0;
+    cin>>n;
+    int a=0;
+    int b=1;
+    int c=0;
+        cout << 1 << endl;
+        for (int i = 1; i<= n; i++) {
+            int c = 0;
+            c = a + b;
+            a=b;
+            b=c;
+           if(c<=n){
+               cout << c << endl;
+           } else{
+               break;
+           }
+        }
+    return 0;
 }
